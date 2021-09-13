9a262340b9ffed480d08abbd8f60ff550a4a7fe4
Thomas Dokas <dokastho@umich.edu>
2021-09-13 12:22:42 -0400

    done

--- stores.cpp
+++ stores.cpp
@@ -8,59 +8,60 @@
 
 using namespace std;
 
+int find_furthest(vector<pair<int,int>> &vec, int r, int c) {
+    int maxDist = 0, dist = 0;
+    for (size_t i = 0; i < vec.size(); i++)
+    {
+        dist = abs(r - vec[i].first) + abs(c - vec[i].second);
+        maxDist = max(maxDist,dist);
+    }
+    return maxDist;
+}
+
 int solution(int K, vector< vector<int> > &A) {
     // write your code in C++14 (g++ 6.2.0)
-    vector<vector<bool>> layers(A.size(),vector<bool>(A.front().size(),true));
+    vector<pair<int,int>> houses;
+    size_t total = 0;
     for (int row = 0; row < (int)A.size(); row++)
     {
         for (int col = 0; col < (int)A[row].size(); col++)
         {
             if (A[row][col] == 1)
             {
-                vector<vector<bool>> layer(A.size(),vector<bool>(A.front().size(),false));
-                for (int i = max(0,row - K); i < min((int)A.size(),row + K + 1); i++)
-                {
-                    int startingCol = col - (K - abs(i - row)), endingCol =  min((int)A.front().size(),col + (abs(startingCol - col) + 1));
-                    startingCol = max(0,startingCol);
-                    fill(layer[i].begin() + startingCol,layer[i].begin() + endingCol, true);
-                    
-                }
-                for (size_t layerRow = 0; layerRow < layers.size(); layerRow++)
-                {
-                    for (size_t layerCol = 0; layerCol < layers[layerRow].size(); layerCol++)
-                    {
-                        layers[layerRow][layerCol] = layers[layerRow][layerCol] && layer[layerRow][layerCol];
-                    }
-                }
+                houses.push_back({row,col});
             }
         }
     }
-    size_t total = 0;
-    for (size_t row = 0; row < layers.size(); row++)
+    for (int row = 0; row < (int)A.size(); row++)
     {
-        for (size_t col = 0; col < layers[row].size(); col++)
+        for (int col = 0; col < (int)A[row].size(); col++)
         {
-            if (layers[row][col] && A[row][col] == 0)
+            if (A[row][col] == 1)
             {
+                continue;
+            }
+            
+            if (find_furthest(houses,row,col) <= K) {
                 total++;
             }
         }
     }
+    
     return total;
 }
 int main() {
     vector<vector<int>> test = {
-        {0,0,0,1},
-        {0,1,0,0},
-        {0,0,1,0},
-        {1,0,0,0},
-        {0,0,0,0}
+        // {0,0,0,1},
+        // {0,1,0,0},
+        // {0,0,1,0},
+        // {1,0,0,0},
+        // {0,0,0,0}
         // {0,0,0,0},
         // {0,0,1,0},
         // {1,0,0,1}
-        // {0,1},
-        // {0,0}
+        {0,1},
+        {0,0}
     };
-    int k = 4;
+    int k = 1;
     cout << solution(k,test);
 }
